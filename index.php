c1bbd024960c24f5123470eec3b3b962c89d9fc8
Hloni <hloniletsaba@gmail.com>
2025-08-28 16:54:24 +0200

    first commit

---
+++ index.php
@@ -0,0 +1,35 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Document</title>
+</head>
+<body>
+    <h1>Marathon</h1>
+    <br>
+    <label for="total_distance">Total Marathon Distance (km):</label>
+    <input type="number" name="total_distance" id="total_distance" />
+    <br>
+    <label for="distance_covered">Distance Already Covered (km):</label>
+    <input type="number" name="distance_covered" id="distance_covered" />
+    <br>
+    <label for="elapsed_time">Elapsed Time Since Start (hours):</label>
+    <input type="number" name="elapsed_time" id="elapsed_time" />
+    <br>
+    <label for="target_time">Target Time To Complete Marathon (hours):</label>
+    <input type="number" name="target_time" id="target_time" />
+    <br>
+    <br>
+    <button> Calculate </button>
+    <hr>
+    <h2> Calculations Results: </h2>
+    <br>
+    
+    
+
+
+
+    
+</body>
+</html>
